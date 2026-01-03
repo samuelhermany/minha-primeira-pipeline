a786a04efe5b92b3a4d038d7f556f1f5772347d8
Samuel Hermany <samuelhermany@hotmail.com>
2026-01-03 20:55:52 +0000

    :fix:alterado ip para testes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-const URL = "http://54.226.49.219:3000";
+const URL = "http://3.85.184.66:3000";
 function App() {
   const [data, setData] = useState([]);
   const [inputValue, setInputValue] = useState("");
